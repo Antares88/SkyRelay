8d5216ada64dc1d02cdec43a1e05830fecaa9e12
YJ <hanul@hanul.me>
2017-04-27 18:53:08 +0900

    UPPERCASE 2.0 반영

--- SkyRelay.js
+++ SkyRelay.js
@@ -1,148 +1,89 @@
-require('./UJS-NODE.js');
+require('uppercase-core');
 
-CPU_CLUSTERING(function() {
-	'use strict';
+CPU_CLUSTERING(() => {
 	
-	var
-	//IMPORT: dgram
-	dgram = require('dgram'),
-	
-	// config
-	config = JSON.parse(READ_FILE({
+	let config = JSON.parse(READ_FILE({
 		path : 'config.json',
 		isSync : true
-	})),
+	}));
 	
-	// shared waiter store
-	sharedWaiterStore = SHARED_STORE('sharedWaiterStore'),
+	let sharedWaiterStore = SHARED_STORE('sharedWaiterStore');
 	
-	// server
-	server;
-	
-	server = dgram.createSocket('udp4');
-	
-	server.on('message', function(message, info) {
+	let server = UDP_SERVER(config.port, (requestInfo, content, response) => {
 		
-		var
-		// content
-		content = message.toString(),
+		let index;
 		
-		// ip
-		ip = info.address,
-		
-		// port
-		port = info.port,
-		
-		// index
-		index,
-		
-		// data
-		data,
-		
-		// waiter info
-		waiterInfo,
-		
-		// send.
-		send = function(data, ip, port) {
-			
-			var
-			// message
-			message;
-			
-			message = new Buffer(CHECK_IS_DATA(data) === true ? STRINGIFY(data) : data);
-			
-			if (port > 0 && port < 65536) {
-				server.send(message, 0, message.length, port, ip);
-			}
-		},
-		
-		// response.
-		response = function(data) {
-			
-			var
-			// message
-			message;
-			
-			message = new Buffer(CHECK_IS_DATA(data) === true ? STRINGIFY(data) : data);
-			
-			server.send(message, 0, message.length, port, ip);
-		};
-		
-		while (( index = content.indexOf('\r\n')) !== -1) {
+		while ((index = content.indexOf('\r\n')) !== -1) {
 
-			data = PARSE_STR(content.substring(0, index));
+			let data = PARSE_STR(content.substring(0, index));
 			
 			if (data !== undefined) {
-				break;
+				
+				// order
+				if (data.methodName === 'order') {
+					
+					data.enemyIP = ip;
+					data.enemyPort = port;
+					
+					server.send({
+						ip : ip,
+						port : port,
+						content : new Buffer(CHECK_IS_DATA(data) === true ? STRINGIFY(data) : data)
+					});
+				}
+				
+				// check player waiting
+				else if (data.methodName === 'checkPlayerWaiting') {
+					
+					let waiterInfo = sharedWaiterStore.get('waiter-' + data.version + '-' + data.roomId);
+					
+					if (waiterInfo !== undefined) {
+						response(STRINGIFY(waiterInfo));
+					}
+				}
+				
+				// for find player
+				else if (data.methodName === 'findPlayer') {
+					
+					let waiterInfo = sharedWaiterStore.get('waiter-' + data.version + '-' + data.roomId);
+					
+					// wait.
+					if (waiterInfo === undefined) {
+						
+						response('waiting');
+						
+						sharedWaiterStore.save({
+							id : 'waiter-' + data.version + '-' + data.roomId,
+							data : {
+								ip : ip,
+								port : port,
+								localIP : data.localIP,
+								localPort : data.localPort
+							},
+							removeAfterSeconds : 3
+						});
+					}
+					
+					// if waiter
+					else if (waiterInfo.ip === ip && waiterInfo.port === port && waiterInfo.localIP === data.localIP && waiterInfo.localPort === data.localPort) {
+						response('waiting');
+					}
+					
+					// match.
+					else {
+						
+						send('knock', waiterInfo.ip, waiterInfo.port);
+						
+						response(STRINGIFY(waiterInfo));
+						
+						sharedWaiterStore.remove('waiter');
+					}
+				}
 			}
 
 			content = content.substring(index + 1);
 		}
-		
-		if (data !== undefined) {
-			
-			// order
-			if (data.methodName === 'order') {
-				
-				data.enemyIP = ip;
-				data.enemyPort = port;
-				
-				send(data, data.ip, data.port);
-			}
-			
-			// check player waiting
-			else if (data.methodName === 'checkPlayerWaiting') {
-				
-				waiterInfo = sharedWaiterStore.get('waiter-' + data.version + '-' + data.roomId);
-				
-				if (waiterInfo !== undefined) {
-					response(waiterInfo);
-				}
-			}
-			
-			// for find player
-			else if (data.methodName === 'findPlayer') {
-				
-				waiterInfo = sharedWaiterStore.get('waiter-' + data.version + '-' + data.roomId);
-				
-				// wait.
-				if (waiterInfo === undefined) {
-					
-					response('waiting');
-					
-					sharedWaiterStore.save({
-						name : 'waiter-' + data.version + '-' + data.roomId,
-						value : {
-							ip : ip,
-							port : port,
-							localIP : data.localIP,
-							localPort : data.localPort
-						},
-						removeAfterSeconds : 3
-					});
-				}
-				
-				// if waiter
-				else if (waiterInfo.ip === ip && waiterInfo.port === port && waiterInfo.localIP === data.localIP && waiterInfo.localPort === data.localPort) {
-					response('waiting');
-				}
-				
-				// match.
-				else {
-					
-					send('knock', waiterInfo.ip, waiterInfo.port);
-					
-					response(waiterInfo);
-					
-					sharedWaiterStore.remove('waiter');
-				}
-			}
-		}
 	});
 	
-	server.on('listening', function() {
-		console.log('RUNNING SKY RELAY... (PORT:' + config.port + ')');
-	});
-	
-	server.bind(config.port);
+	console.log(CONSOLE_GREEN('Sky Relay 서버를 실행합니다. (포트:' + config.port + ')'));
 });
